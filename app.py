9f493f481ee5d43a97c313888d55f3b0cf3e53d8
SHREYAS RAVEENDRANATH <95408044+ShreyasRaveendranath@users.noreply.github.com>
2023-05-15 21:26:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import base64
 import streamlit as st
+import plotly.express as px
 from PIL import Image
 import pandas as pd
 from pickle import load
@@ -6,15 +8,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from bs4 import BeautifulSoup
 import re
-import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-nltk.download('stopwords')
-nltk.download('wordnet')
 from sklearn.model_selection import train_test_split
 import time
-
-
+import nltk
 # Set the page configuration with the title and background image
 st.set_page_config(
     page_title="Condition and Drug Name Prediction",
@@ -22,7 +20,6 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded"
 )
-
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -32,6 +29,7 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
+
 # Load the trained model and vectorizer
 model = load(open('model.pkl', 'rb'))
 vectorizer = load(open('vectorizer.pkl', 'rb'))
@@ -40,27 +38,92 @@ vectorizer = load(open('vectorizer.pkl', 'rb'))
 st.markdown(
     """
     <div style="text-align: center;">
-        <h1 style="color: orange; font-size: 50px;">Welcome to our app!</h1>
-        <p style="font-size: 18px;">Enter your review and we'll predict the condition and recommend drugs for you.</p>
+        <h1 style="color: black; font-size: 50px;">Welcome to our app!</h1>
+        <p style="color: silver; font-size: 18px;">Enter your review and we'll predict the condition and recommend drugs for you.</p>
     </div>
     """,
     unsafe_allow_html=True
 )
-# Load the image
-image = Image.open("background.jpg")
 
-# Reduce the image size
-max_size = (800, 600)  # Set the maximum size for the image
-image.thumbnail(max_size)
+df = px.data.iris()
 
-# Display the image
-st.image(image, use_column_width=True)
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-# Create text input for user to enter review
-text = st.text_area('Enter your review:', height=25)
+
+img = get_img_as_base64("image.jpg")
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://i.gifer.com/94aW.gif");
+background-size: 100%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+maxlength_script = """
+<script>
+    const textarea = document.querySelector('textarea');
+    const maxlength = %d;
+
+    textarea.addEventListener('input', function(event) {
+        const currentValue = event.target.value;
+        if (currentValue.length > maxlength) {
+            event.target.value = currentValue.slice(0, maxlength);
+        }
+    });
+</script>
+"""
+
+# Maximum character limit
+max_length = 100
+
+# Add the JavaScript code to the Streamlit app
+st.write(maxlength_script % max_length, unsafe_allow_html=True)
+
+# Create the text area with the maximum character limit
+text = st.text_area('Enter your review:', height=5)
+
+# Display the remaining character count
+
 
 # Create predict button to predict condition and recommended drugs
-if st.button('Predict'):
+
+button_style = '''
+    <style>
+        .predict-button {
+            background-color: black;
+            color: gold;
+            border-color: red;
+        }
+    </style>
+'''
+st.markdown(button_style, unsafe_allow_html=True)
+
+if st.markdown('<button class="predict-button">Predict</button>', unsafe_allow_html=True):
     if not text:
         st.warning("Please enter a review.")
     else:
@@ -84,14 +147,14 @@ if st.button('Predict'):
         # Display loading spinner animation
         with st.spinner('Predicting...'):
             # Simulate prediction delay
-            time.sleep(3)
+            time.sleep(0.5)
 
             # Make prediction using the model
             pred1 = model.predict(tfidf_review)[0]
 
         # Display predicted condition
         st.subheader("Condition:")
-        st.info(pred1)
+        st.markdown(f'<p style="color: gold;">{pred1}</p>', unsafe_allow_html=True)
 
         # Load the data and get recommended drugs
         df2 = pd.read_table('drugsCom_raw (1).tsv')
@@ -101,4 +164,4 @@ if st.button('Predict'):
         # Display recommended drugs
         st.subheader("Recommended Drugs:")
         for i, drug in enumerate(recommended_drugs):
-            st.write(f"{i+1}. {drug}")
+            st.write(f'<span style="color: gold;">{i+1}. {drug}</span>', unsafe_allow_html=True)
